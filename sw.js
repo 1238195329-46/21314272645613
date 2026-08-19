1fe833ecd4804585ffe4a1c76cf0be9676a51f1f
1238195329-46 <fhjeke0@gmail.com>
2026-08-18 22:25:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,23 @@
-const CACHE_NAME = 'balatro-v2';
-const urlsToCache = [
-    './',
-    './index.html',
-    './love.min.js',
-    './module.part1.js',
-    './module.part2.js',
-    './module.part3.js',
-    './module.part4.js',
-    './module.part5.js',
-    './module.part6.js',
-    './module.part7.js',
-    './game.love'
+const CACHE_NAME = 'balatro-v1';
+const FILES = [
+    './', './index.html', './love.min.js',
+    './module.part1.js', './module.part2.js', './module.part3.js',
+    './module.part4.js', './module.part5.js', './module.part6.js',
+    './module.part7.js', './game.love'
 ];
 
-// Install: Cache all assets
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Cache opened');
-                return cache.addAll(urlsToCache);
-            })
-            .catch(err => console.log('Cache failed:', err))
-    );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
     self.skipWaiting();
 });
 
-// Fetch: Serve from cache, fallback to network
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Return cached version or fetch new
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
 
-// Activate: Clean up old caches
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
-            );
-        })
-    );
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => 
+        Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ));
     self.clients.claim();
 });
